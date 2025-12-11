3c04e2d112df3b79536ef353a974368f9b1a5e1a
Martin Simango <shukomango@gmail.com>
2025-12-11 23:27:21 +0200

    feat: add proto file for sst block entry

--- internal/db/sst.go
+++ internal/db/sst.go
@@ -1,6 +1,17 @@
+//go:generate protoc --go_out=. sst.proto
 package db
 
-import "os"
+import (
+	"context"
+	"os"
+)
+
+type SSTable interface {
+	Flush(memTable []MemTableData) (uint32, error)
+	Get(ctx context.Context, key string) (string, bool)
+	GetPrefix(ctx context.Context, prefix string) map[string]string
+	Close() error
+}
 
 // sst.go
 // This file would contain the implementation details for SSTable operations
@@ -44,9 +55,6 @@ const (
 // restart count (uint16) - number of restart points
 // checksum (uint32) - crc32 checksum of the block
 
-type DataBlockEntry struct {
-}
-
 // index block entry structure in file
 // [key (bytes)][data block offset (uint32)][data block size (uint32)][re]
 
@@ -80,16 +88,74 @@ type SSTableBlock struct {
 // index block offset
 // index block size
 
-type SSTable struct {
-	file *os.File
+type fileSSTable struct {
+	*os.File
+	filename string
 }
 
-func newSSTable() *SSTable {
-	return &SSTable{}
+var _ SSTable = (*fileSSTable)(nil)
+
+// Create creates a new SSTable file with the given filename.
+func Create(filename string) (SSTable, error) {
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return nil, err
+	}
+	sst := &fileSSTable{
+		File:     f,
+		filename: filename,
+	}
+	return sst, nil
 }
 
-// Write memtable data to SSTable and return the number of records written
-func (sst *SSTable) write(memTable []MemTableData) (uint32, error) {
+// func (w *WalFile) WriteRecord(record *WalRecord) error {
+// 	w.mu.Lock()
+// 	defer w.mu.Unlock()
+// 	// make size of record is 2^16 bytes - 65536 bytes - limit set by simpledb
+// 	if err := binary.Write(w.file, binary.LittleEndian, uint16(proto.Size(record))); err != nil {
+// 		return err
+// 	}
+// 	bytes, err := proto.Marshal(record)
+// 	if err != nil {
+// 		return err // TODO: now file is corrupted as size is written but data is not - remediate this
+// 	}
+// 	_, err = w.file.Write(bytes)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
+
+// Flush flushes memtable data to SSTable and returns the number of records written
+func (sst *fileSSTable) Flush(memTable []MemTableData) (uint32, error) {
+	// create new SSTable file
+	// write data blocks
+	// write index block
+	// write footer
+
+	// offset := uint32(0)
+	// restartPoints := make([]uint32, 0)
+
+	// for i, m := range memTable {
+
+	// 	// record := &BlockEntry{
+	// 	// 	UnsharedKey: ,
+	// 	// }
+
+	// 	// write data block entry
+	// 	record.sharedKeyCount = 0
+	// 	record.unsharedKeyCount = uint64(len(m.Key))
+	// 	record.valueLen = uint64(len(m.Value))
+	// 	bytes, err := proto.Marshal()
+	// 	if err != nil {
+	// 		return 0, err
+	// 	}
+	// 	_, err = sst.Write(bytes)
+	// 	if err != nil {
+	// 		return 0, err
+	// 	}
+	// 	// write data block entries
+	// }
 
 	// algorithm:
 	// Write data blocks
@@ -103,7 +169,20 @@ func (sst *SSTable) write(memTable []MemTableData) (uint32, error) {
 	// Write footer
 
 	return 0, nil
+}
 
+// Get implements [SSTable].
+func (sst *fileSSTable) Get(ctx context.Context, key string) (string, bool) {
+	panic("unimplemented")
+}
+
+// GetPrefix implements [SSTable].
+func (sst *fileSSTable) GetPrefix(ctx context.Context, prefix string) map[string]string {
+	panic("unimplemented")
+}
+
+func (sst *fileSSTable) Close() error {
+	return sst.File.Close()
 }
 
 // func (sst *SSTable) read(key string) (string, bool) {
